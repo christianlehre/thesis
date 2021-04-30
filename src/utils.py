6de12f82cff38f0081e2c8b53df0ef28facd6bce
christianlehre <lehre.christian@gmail.com>
2021-04-30 15:29:44 +0200

    add functino that scales the figsize to avoid scaling in latex

--- src/utils.py
+++ src/utils.py
@@ -67,3 +67,38 @@ def coverage_probability(y_test, lower_ci, upper_ci):
     coverage = num_samples_inside_ci / num_samples
 
     return coverage
+
+
+def set_size(width, fraction=1):
+    """Set figure dimensions to avoid scaling in LaTeX.
+
+    Parameters
+    ----------
+    width: float
+            Document textwidth or columnwidth in pts
+    fraction: float, optional
+            Fraction of the width which you wish the figure to occupy
+
+    Returns
+    -------
+    fig_dim: tuple
+            Dimensions of figure in inches
+    """
+    # Width of figure (in pts)
+    fig_width_pt = width * fraction
+
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+    # Golden ratio to set aesthetic figure height
+    # https://disq.us/p/2940ij3
+    golden_ratio = (5**.5 - 1) / 2
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio
+
+    fig_dim = (fig_width_in, fig_height_in)
+
+    return fig_dim
